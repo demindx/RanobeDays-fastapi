cffa3dc3d1381b02b7023809ad749e2a70a3a6c4
demindx <oliver9574@gmail.com>
2025-07-12 20:42:02 +0200

    fix(core): fixed typehinting, imports and added session commit on dependency

--- src/core/database.py
+++ src/core/database.py
@@ -1,18 +1,21 @@
+from typing import AsyncGenerator
+
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from src.config import config
-from src.models import Base
+from src.core.models import Base
 
 engine = create_async_engine(config.POSTGRES_URL)
 
 sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def get_db_session() -> AsyncSession:
+async def get_db_session() -> AsyncGenerator[AsyncSession]:
     async with sessionmaker() as session:
         try:
             yield session
+            await session.commit()
         except Exception:
             await session.rollback()
             raise
@@ -23,5 +26,5 @@ async def get_db_session() -> AsyncSession:
 async def init_db() -> None:
     async with engine.begin() as conn:
         await conn.execute(text("create extension if not exists vector;"))
-        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
