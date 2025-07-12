2afa9322c3535ff3d90a2255afe198cf6a505a0b
demindx <oliver9574@gmail.com>
2025-07-12 20:40:58 +0200

    fix(core): basedatatime model fixed naive time

--- src/core/models.py
+++ src/core/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 
+from sqlalchemy import DateTime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -7,7 +8,7 @@ class Base(DeclarativeBase): ...
 
 
 class BaseTimestamps:
-    created_at: Mapped[datetime | None] = mapped_column(default=lambda: datetime.now(timezone.utc))
-    updated_at: Mapped[datetime | None] = mapped_column(
-        default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc)
     )
