a44fab376ced211d35bc313a8c783143316e9b6d
demindx <oliver9574@gmail.com>
2025-11-04 11:14:33 +0100

    feat(users): rewrite to use abstract service and repository

--- src/users/dependencies.py
+++ src/users/dependencies.py
@@ -3,18 +3,30 @@ from typing import Annotated
 from fastapi import Depends
 
 from src.core.dependencies import DbSession
-from src.users.repository import UserRepository
-from src.users.service import UserService
+from src.users.models import User, UserProfile
+from src.users.repository import UserProfileRepository, UserRepository
+from src.users.service import UserProfileService, UserService
 
 
 def get_user_repo(session: DbSession) -> UserRepository:
-    return UserRepository(session)
+    return UserRepository(session, User)
+
+
+def get_user_profile_repo(session: DbSession) -> UserProfileRepository:
+    return UserProfileRepository(session, UserProfile)
 
 
 def get_user_service(
-    repo: Annotated[UserRepository, Depends(get_user_repo)],
+    repo: Annotated[UserRepository, Depends(get_user_repo)]
 ) -> UserService:
     return UserService(repo)
 
 
+def get_user_profile_service(
+    repo: Annotated[UserProfileRepository, Depends(get_user_profile_repo)]
+) -> UserProfileService:
+    return UserProfileService(repo)
+
+
 UserServiceDep = Annotated[UserService, Depends(get_user_service)]
+UserProfileServiceDep = Annotated[UserProfileService, Depends(get_user_profile_service)]
--- src/users/service.py
+++ src/users/service.py
@@ -1,93 +1,30 @@
-from collections.abc import Sequence
-
-from src.core.exceptions import AlreadyExists, NotFound
-from src.users.models import User, UserProfile
-from src.users.repository import UserRepository
+from src.users.repository import UserProfileRepository, UserRepository
 from src.users.schemas import (
-    UserProfileUpdateRequest,
-    UserRegisterRequest,
+    UserProfileUpdate, 
+    UserRegister, 
+    UserPasswordUpdate, 
+    UserProfileCreate
 )
-from src.users.utils import get_password_hash
+from src.users.models import User, UserProfile
+from src.core.service import AbstractService
 
 
-class UserService:
-    def __init__(self, repository: UserRepository):
-        self.repository: UserRepository = repository
+class UserService(AbstractService[
+                  User, 
+                  UserRegister,
+                  UserPasswordUpdate, UserRepository]):
+    async def get_by_login(self, login: str) -> User:
+        return await self.repo.get_by_login(login)
 
-    async def create_user(self, data: UserRegisterRequest) -> User:
-        user_exists = await self.repository.get_user_by_login(data.login)
+    async def get_by_email(self, email: str) -> User:
+        return await self.repo.get_by_email(email)
 
-        if user_exists:
-            raise AlreadyExists(User)
 
-        user = User(
-            login=data.login,
-            email=data.email,
-            password_hash=get_password_hash(data.get_password()),
-        )
-
-        await self.repository.create_user(user)
-
-        profile = UserProfile(user_id=user.id)
-
-        await self.repository.create_user_profile(profile)
-
-        return user
-
-    async def get_user(self, id: int) -> User:
-        user = await self.repository.get_user(id)
-
-        if not user:
-            raise NotFound(User, "id", id)
-
-        return user
-
-    async def get_all_users(self) -> Sequence[User]:
-        users = await self.repository.get_all_users()
-
-        return users
-
-    async def get_user_profile(self, user_id: int) -> UserProfile:
-        profile = await self.repository.get_user_profile(user_id)
-
-        if not profile:
-            raise NotFound(UserProfile, "id", user_id)
-
-        return profile
-
-    async def update_user_profile(
-        self, user_id: int, data: UserProfileUpdateRequest
-    ) -> UserProfile:
-        profile = await self.repository.get_user_profile(user_id)
-        if not profile:
-            raise NotFound(UserProfile, "id", user_id)
-
-        for attr, value in data.model_dump(
-            exclude_unset=True, exclude_none=True
-        ).items():
-            if hasattr(profile, attr):
-                setattr(profile, attr, value)
-
-        profile = await self.repository.update_user_profile(profile)
-
-        return profile
-
-    async def _update_user_status(self, id: int, is_active: bool) -> User:
-        user = await self.repository.get_user(id)
-        if not user:
-            raise NotFound(User, "id", id)
-
-        user.is_active = is_active
-
-        user = await self.repository.update_user(user)
-
-        return user
-
-    async def activate_user(self, id: int) -> User:
-        return await self._update_user_status(id, True)
-
-    async def deactivate_user(self, id: int) -> User:
-        return await self._update_user_status(id, False)
-
-    async def delete_user(self, id: int) -> None:
-        await self.repository.delete_user(id)
+class UserProfileService(AbstractService[
+                         UserProfile,
+                         UserProfileCreate,
+                         UserProfileUpdate,
+                         UserProfileRepository
+                         ]):
+    async def get_by_user_id(self, user_id: int) -> UserProfile:
+        return await self.repo.get_by_user_id(user_id)
