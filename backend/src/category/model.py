00b6568892912b789959786912ff4455bfe70ed0
demindx <oliver9574@gmail.com>
2026-02-06 11:10:54 +0100

    misspelling

--- backend/src/category/model.py
+++ backend/src/category/model.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.core.models import Base
 
 if TYPE_CHECKING:
-    from src.chapters.schemas import ChapterCreate  # noqa: F401
+    from src.chapter.schemas import ChapterCreate  # noqa: F401
 
 
 class CategoryTypeEnum(Enum):
